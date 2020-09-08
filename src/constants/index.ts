8a342c2adc90e888bad1e6be7d26f5d144029103
guanbinrui <guanbinrui@dimension.im>
2020-09-08 17:35:40 +0800

    chore: update router contract addr

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x11bdf0b08a714b34be1ed0b0472f054a053cbe2d'
 
 // a list of tokens by chain
 type ChainTokenList = {
